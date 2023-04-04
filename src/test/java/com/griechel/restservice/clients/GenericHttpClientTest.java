6ae470dffdaef014988df88e58555623d6a3855a
Garren Riechel <riechel@amazon.com>
2023-04-04 17:04:32 -0600

    Use generic string response

--- src/test/java/com/griechel/restservice/clients/GenericHttpClientTest.java
+++ src/test/java/com/griechel/restservice/clients/GenericHttpClientTest.java
@@ -1,13 +1,12 @@
 package com.griechel.restservice.clients;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
-import com.griechel.restservice.types.WeatherPointData;
-import com.griechel.restservice.types.WeatherPointResponse;
 
 public class GenericHttpClientTest {
 
@@ -16,13 +15,11 @@ public class GenericHttpClientTest {
 
     @Test
     public void testGet() throws JsonProcessingException {
+        String expectedAverageTemp = "\"tavg\":0.3";
         String weatherKey = System.getenv("WEATHER_KEY");
         String responseString = client.get("?lat=43.6667&lon=-79.4&start=2020-01-01&end=2020-01-01&alt=184",
                 "x-rapidapi-host", "meteostat.p.rapidapi.com",
                 "x-rapidapi-key", weatherKey);
-        WeatherPointResponse response = om.readValue(responseString, WeatherPointResponse.class);
-        assertEquals(1, response.getData().size());
-        WeatherPointData data = response.getData().get(0);
-        assertEquals(0.3, data.getAverageTemp(), 0);
+        assertTrue(responseString.contains(expectedAverageTemp));
     }
 }
